2ebbb3f84931c01e274fe422b13344a7eba52a29
Vasylenko Valeriy <hitehnik132@gmail.com>
2023-04-20 08:45:40 +0300

    added staff model, added task view

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User, Staff, Task, Category
+from .models import Customer, Task, Category, Staff
 
-admin.site.register(User)
+admin.site.register(Customer)
 admin.site.register(Staff)
 admin.site.register(Task)
 admin.site.register(Category)
--- main/models.py
+++ main/models.py
@@ -1,34 +1,40 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 
 
-class User(AbstractUser):
-    username = models.CharField(max_length=200, blank=False, unique=False)
-    email = models.EmailField(unique=True, null=False)
-    city = models.CharField(blank=False, default='Kyiv')
-    street = models.CharField(blank=True)
-    house = models.CharField(blank=True)
-    apartment = models.CharField(blank=True)
-    phone = models.CharField(blank=False)
+class Customer(User):
+    city = models.CharField(max_length=200, blank=False, default='Kyiv')
+    street = models.CharField(max_length=200, blank=True)
+    house = models.CharField(max_length=200, blank=True)
+    apartment = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=200, blank=False)
     is_confirmed = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
+
+
+class Staff(User):
+    city = models.CharField(max_length=200, blank=False, default='Kyiv')
+    street = models.CharField(max_length=200, blank=True)
+    house = models.CharField(max_length=200, blank=True)
+    apartment = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=200, blank=False)
 
 
 class Confirmations(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
     confirmation_key = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Category(models.Model):
-    name = models.CharField(null=False)
-    description = models.CharField(blank=True)
+    name = models.CharField(max_length=200, null=False)
+    description = models.CharField(max_length=1000, blank=True)
 
 
 class Task(models.Model):
-    client = models.ForeignKey(User, on_delete=models.CASCADE)
-    employee = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    client = models.ForeignKey(Customer, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=1000, blank=False)
     created = models.DateTimeField(auto_now_add=True)
+    in_work = models.BooleanField(default=False)
     finished = models.BooleanField(default=False)
+    employee = models.ForeignKey(Staff, on_delete=models.CASCADE, null=True, blank=True)
