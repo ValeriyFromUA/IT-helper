eccfa312181c3750b30df0572ad0431028838cab
Vasylenko Valeriy <hitehnik132@gmail.com>
2023-05-06 07:57:43 +0300

    fixed middleware

--- helper/middleware.py
+++ helper/middleware.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect
 
-NOT_ALLOWED_URLS = ["/edit_profile/", "/new_task/", ]
-ONLY_STAFF_URLS = ["/staff/task/", "/staff/main/", "new_price"]
+NOT_ALLOWED_URLS = ["/edit_profile/", "/new_task/", "/staff/task/", "/staff/main/", "/new_price/"]
+ONLY_STAFF_URLS = ["/staff/task/", "/staff/main/", "/new_price/"]
 
 
 class ConfirmUserMiddleware:
@@ -9,11 +9,15 @@ class ConfirmUserMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        print(request.path)
-        if request.user.is_authenticated and not request.user.is_confirmed:
-            if not request.user.is_staff and request.path in ONLY_STAFF_URLS:
+        # print(request.path)
+        if request.user.is_authenticated:
+            if not request.user.is_confirmed:
+                if request.path in NOT_ALLOWED_URLS:
+                    return redirect("/confirm/")
+                elif request.path in ONLY_STAFF_URLS:
+                    return redirect("/home/")
+        if not request.user.is_staff:
+            if request.path in ONLY_STAFF_URLS:
                 return redirect("/home/")
-            if request.path in NOT_ALLOWED_URLS:
-                return redirect("/confirm/")
         response = self.get_response(request)
         return response
